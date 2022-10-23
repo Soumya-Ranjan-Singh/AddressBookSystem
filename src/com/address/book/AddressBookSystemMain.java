64fa45902fbf7409bc32d56899dfc2ef0a07cb4c
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-24 03:16:02 +0530

    Added : Display Welcome to Address Book Program

---
+++ src/com/address/book/AddressBookSystemMain.java
@@ -0,0 +1,22 @@
+//This program is to display Welcome to Address Book System in main class.
+
+package com.address.book;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookSystemMain {
+
+    //Default Constructor
+    public AddressBookSystemMain()
+    {
+        System.out.println("Welcome to Address Book Program !!!");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize Object
+        AddressBookSystemMain obj = new AddressBookSystemMain();
+
+    }
+}
